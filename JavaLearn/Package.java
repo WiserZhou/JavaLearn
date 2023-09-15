9450fa819035f4053513155d08758b1e9f5ce179
WiserZhou <wiserx@qq.com>
2023-09-15 14:46:49 +0800

    java包开始学习

---
+++ JavaLearn/Package.java
@@ -0,0 +1,11 @@
+package JavaLearn;
+
+public class Package {
+  public Package() {
+    System.out.println("package");
+  }
+
+  public static void main(String[] args) {
+    JavaLearn.Package pa = new JavaLearn.Package();
+  }
+}
