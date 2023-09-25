b49ecd368b9cc24582daa1c49b168090244e9072
WiserZhou <wiserx@qq.com>
2023-09-25 09:36:26 +0800

    第五章结束

---
+++ Packaging.java
@@ -0,0 +1,17 @@
+public class Packaging {
+  public static void main(String[] args) {
+    //    int num = 42;
+    Integer intObj = 12;
+    int num = intObj;
+    String str = "123";
+    int num2 = Integer.parseInt(str);
+    String str2 = intObj.toString();
+    Integer intObj2 = 22;
+    int result = intObj.compareTo(intObj2);
+    System.out.println(result);
+    int max = Integer.MAX_VALUE;
+    int min = Integer.MIN_VALUE;
+    System.out.println(max);
+    System.out.println(min);
+  }
+}
