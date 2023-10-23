765c245636e28ef96b000e401875b9373ccbe32c
WiserZhou <wiserx@qq.com>
2023-10-23 09:32:19 +0800

    Java多线程锁

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ThreadTest {
   public static void main3(String[] args) {
     Thread thread = new MyThread();
@@ -39,7 +43,7 @@ public class ThreadTest {
     t.start(); // 启动新线程
   }
 
-  public static void main(String[] args) throws Exception {
+  public static void main6(String[] args) throws Exception {
     var add = new AddThread();
     var dec = new DecThread();
     add.start();
@@ -47,6 +51,65 @@ public class ThreadTest {
     add.join();
     dec.join();
     System.out.println(Counter.count);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    var q = new TaskQueue();
+    var ts = new ArrayList<Thread>();
+    for (int i = 0; i < 5; i++) {
+      var t =
+          new Thread() {
+            public void run() {
+              // 执行task:
+              while (true) {
+                try {
+                  String s = q.getTask();
+                  System.out.println("execute task: " + s);
+                } catch (InterruptedException e) {
+                  return;
+                }
+              }
+            }
+          };
+      t.start();
+      ts.add(t);
+    }
+    var add =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 10; i++) {
+                // 放入task:
+                String s = "t-" + Math.random();
+                System.out.println("add task: " + s);
+                q.addTask(s);
+                try {
+                  Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+              }
+            });
+    add.start();
+    add.join();
+    Thread.sleep(100);
+    for (var t : ts) {
+      t.interrupt();
+    }
+  }
+}
+
+class TaskQueue {
+  Queue<String> queue = new LinkedList<>();
+
+  public synchronized void addTask(String s) {
+    this.queue.add(s);
+    this.notifyAll();
+  }
+
+  public synchronized String getTask() throws InterruptedException {
+    while (queue.isEmpty()) {
+      this.wait();
+    }
+    return queue.remove();
   }
 }
 
@@ -60,6 +123,7 @@ class AddThread extends Thread {
     for (int i = 0; i < 10000; i++) {
       synchronized (Counter.lock) {
         Counter.count += 1;
+        System.out.println("add" + Counter.count);
       }
     }
   }
@@ -70,6 +134,7 @@ class DecThread extends Thread {
     for (int i = 0; i < 10000; i++) {
       synchronized (Counter.lock) {
         Counter.count -= 1;
+        System.out.println("dec" + Counter.count);
       }
     }
   }
