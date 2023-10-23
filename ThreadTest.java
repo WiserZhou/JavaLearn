b2e0e7f91fcf3a50cb3887c07c21aa69ca6b12c0
WiserZhou <wiserx@qq.com>
2023-10-23 08:59:07 +0800

    Java多线程

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,6 +1,126 @@
 public class ThreadTest {
-    public static void main(String[]args){
-        Thread thread =new Thread("hello");
+  public static void main3(String[] args) {
+    Thread thread = new MyThread();
+    thread.start();
+  }
 
+  public static void main5(String[] args) {
+    System.out.println("main start...");
+    Thread t =
+        new Thread() {
+          public void run() {
+            System.out.println("thread run...");
+            try {
+              Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("thread end.");
+          }
+        };
+    t.start();
+    try {
+      Thread.sleep(20);
+    } catch (InterruptedException e) {
     }
+    System.out.println("main end...");
+  }
+
+  public static void main4(String[] args) {
+    Thread thread = new Thread(new MyRunnable());
+    thread.start();
+  }
+
+  public static void main2(String[] args) {
+    Thread t =
+        new Thread(
+            () -> {
+              System.out.println("start new thread!");
+            });
+    t.start(); // 启动新线程
+  }
+
+  public static void main(String[] args) throws Exception {
+    var add = new AddThread();
+    var dec = new DecThread();
+    add.start();
+    dec.start();
+    add.join();
+    dec.join();
+    System.out.println(Counter.count);
+  }
+}
+
+class Counter {
+  public static final Object lock = new Object();
+  public static int count = 0;
+}
+
+class AddThread extends Thread {
+  public void run() {
+    for (int i = 0; i < 10000; i++) {
+      synchronized (Counter.lock) {
+        Counter.count += 1;
+      }
+    }
+  }
+}
+
+class DecThread extends Thread {
+  public void run() {
+    for (int i = 0; i < 10000; i++) {
+      synchronized (Counter.lock) {
+        Counter.count -= 1;
+      }
+    }
+  }
+}
+
+class HelloThread2 extends Thread {
+  public volatile boolean running = true; // 。线程间共享变量需要使用volatile关键字标记，确保每个线程都能读取到更新后的变量值。
+
+  public void run() {
+    int n = 0;
+    while (running) {
+      n++;
+      System.out.println(n + " hello!");
+    }
+    System.out.println("end!");
+  }
+}
+
+class MyThread extends Thread {
+  public void run() {
+    Thread hello = new HelloThread();
+    hello.start(); // 启动hello线程
+    try {
+      hello.join(); // 等待hello线程结束
+    } catch (InterruptedException e) {
+      System.out.println("interrupted!");
+    }
+    hello
+        .interrupt(); // 在t线程结束前，对hello线程也进行了interrupt()调用通知其中断。如果去掉这一行代码，可以发现hello线程仍然会继续运行，且JVM不会退出。
+  }
+}
+
+class HelloThread extends Thread {
+  public void run() {
+    int n = 0;
+    while (!isInterrupted()) {
+      n++;
+      System.out.println(n + " hello!");
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        System.out.println("Interrupted！");
+        break;
+      }
+    }
+  }
+}
+
+class MyRunnable implements Runnable {
+  @Override
+  public void run() {
+    System.out.println("start new thread!");
+  }
 }
